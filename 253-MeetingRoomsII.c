43cb41fc78c451dbf743f4e255ad2c5267b4c1cd
HansP101 <106307051+HansP101@users.noreply.github.com>
2022-08-30 01:46:28 -0700

    Update 253-MeetingRoomsII.c

--- 253-MeetingRoomsII.c
+++ 253-MeetingRoomsII.c
@@ -15,3 +15,24 @@ public:
         return ans;
     }
 };
+
+
+class Solution {
+public:
+    struct Comparator{  
+        bool operator()(int a, int b){return a>b;} //minHeap 
+    };
+    int minMeetingRooms(vector<vector<int>>& intervals) {
+        int ans=1;
+        priority_queue<int, vector<int>, Comparator> pq;
+        sort(intervals.begin(), intervals.end(), [](auto&a, auto&b){return a[0]==b[0]? a[1]<b[1]: a[0]<b[0];});
+        pq.push(intervals[0][1]);
+        for(int i=1; i<intervals.size(); i++){
+            if(pq.top()<=intervals[i][0]) pq.pop(); //already meeting room was evacuated
+            pq.push(intervals[i][1]);
+            int pqSize=pq.size();
+            ans=max(ans, pqSize);
+        }
+        return ans;
+    }
+};
