5b07f5319aa1aad2460f1de45e3d2a1be13973d7
HansP101 <106307051+HansP101@users.noreply.github.com>
2022-09-01 22:44:45 -0700

    Create 53 Maximum Subarray.c

---
+++ 53 Maximum Subarray.c
@@ -0,0 +1,34 @@
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        //kaden's algorithm
+        int subSum=0;
+        int maxSum=INT_MIN;
+        
+        for(int i=0; i<nums.size(); i++){
+            subSum+=nums[i];
+            if(subSum>maxSum) maxSum=subSum;
+            if(subSum<0)subSum=0;
+        }
+        return maxSum;
+    }
+};
+
+/* burte force
+class Solution{
+    public:
+    int maxSubArray(vector<int>&nums){
+        int maxSum=INT_MIN;
+        for(int i=0; i<nums.size(); i++){
+            int subSum=0;
+            for(int j=i; j<nums.size(); j++){
+                subSum+=nums[j];
+                if(subSum>maxSum) maxSum=subSum;
+            }
+        }
+        return maxSum;
+    }
+};
+
+*/
