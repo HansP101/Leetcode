b25afae3f0322956db7f968fb48acf02092ac950
HansP101 <106307051+HansP101@users.noreply.github.com>
2022-09-10 17:27:53 -0700

    Create 19. Remove Nth Node From End of List.c

---
+++ 19. Remove Nth Node From End of List.c
@@ -0,0 +1,73 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* ans;
+        ListNode* fast=head;
+        ListNode* slow=head;
+        for(int i=0; i<n; i++) fast=fast->next;
+        if(fast==nullptr){
+            ans=head->next;
+            delete head;
+            return ans;
+        }
+        while(fast->next!=nullptr){
+            fast=fast->next;
+            slow=slow->next;
+        }
+        ListNode* temp=slow->next;
+        slow->next=temp->next;
+        delete temp;
+        return head;
+        
+    }
+};
+
+
+
+
+
+//very bad 풀이
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        int lenList=0;
+        ListNode* dummy=new ListNode(-1);
+        dummy->next=head;
+        ListNode* temp=head;
+        while(temp){
+            lenList++;
+            temp=temp->next;
+        }
+        temp=dummy;
+        if(lenList-n==0) {
+            dummy->next=dummy->next->next;
+            return dummy->next;}
+        for(int i=0; i<lenList-n; i++){
+            temp=temp->next;
+        }
+        if(temp->next)
+        temp->next=temp->next->next;
+        return dummy->next;
+    }
+};
