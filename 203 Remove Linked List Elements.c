3024bf1ace91a7f0e492f2b160377f7877cc0f2d
HansP101 <106307051+HansP101@users.noreply.github.com>
2022-09-11 23:17:05 -0700

    Create 203 Remove Linked List Elements.c

---
+++ 203 Remove Linked List Elements.c
@@ -0,0 +1,55 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode* dummyNode=new ListNode(-1);
+        dummyNode->next=head;
+        ListNode* prev=dummyNode;
+        ListNode* cur=dummyNode;
+        while(cur){
+            if(cur->val==val){
+                ListNode* temp=cur;
+                prev->next=cur->next; //editing is here by skipping the node
+                cur=cur->next;
+               delete temp;
+            }else{
+               prev=cur; //prev is following up.
+                cur=cur->next; 
+            }      
+        }
+        return dummyNode->next;
+    }
+};
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode** cur=&head;
+        while(*cur){
+            if((*cur)->val==val){
+                *cur=(*cur)->next; //replacing "itself" to the nextone!
+            }else{
+                cur=&((*cur)->next); //keep traverse to next one!
+            }
+        }
+        return head;
+    }
+};
