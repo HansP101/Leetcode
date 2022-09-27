f3598c02e1324a7e5cd2bd56d125bf655d126123
HansP101 <106307051+HansP101@users.noreply.github.com>
2022-09-26 21:11:21 -0700

    Create 17.Letter Combinations of a Phone Number.c

---
+++ 17.Letter Combinations of a Phone Number.c
@@ -0,0 +1,25 @@
+class Solution {
+public:
+          unordered_map<char, vector<char>> pMap={{'2', {'a', 'b', 'c'}},{'3', {'d', 'e', 'f'}},{'4', {'g','h','i'}},{'5',{'j','k','l'}},{'6',{'m','n','o'}},{'7',{'p','q','r','s'}},{'8',{'t','u','v'}},{'9',{'w','x','y','z'}}};
+   
+        vector<string> letterCombinations(string digits) {
+        if (digits.size()==0) return {};
+        vector<string> ans={};
+        ans.push_back("");
+        for(auto it: digits){
+            BackTracking(it, ans);
+        }
+        return ans;
+    }
+
+    void BackTracking(char c, vector<string>&ans){
+        vector<string> temp;
+        auto it=pMap.find(c);
+        for(auto it2: it->second){
+            for(auto it3: ans){
+                temp.push_back(it3+it2);              
+            } 
+        }   
+        ans=temp;
+    }      
+};
