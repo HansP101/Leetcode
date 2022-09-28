6b776c757006ab62ca47d394b5d5c64a8fb626f5
HansP101 <106307051+HansP101@users.noreply.github.com>
2022-09-27 18:17:39 -0700

    Create 49.Group Anagrams.c

---
+++ 49.Group Anagrams.c
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> anaMap;
+        vector<vector<string>> ans;
+        for(auto it: strs){
+            string org=it;
+            sort(it.begin(), it.end());
+             anaMap[it].push_back(org);              
+        }
+        for(auto it: anaMap){
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
