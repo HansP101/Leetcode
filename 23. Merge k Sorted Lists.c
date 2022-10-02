9a5778a9c557771b9dd0e46a543b5090cb51ff21
HansP101 <106307051+HansP101@users.noreply.github.com>
2022-10-01 20:40:31 -0700

    Create 23. Merge k Sorted Lists.c

---
+++ 23. Merge k Sorted Lists.c
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    ListNode* merge2Lists(ListNode* l1, ListNode* l2) {
+        if (!l1) return l2;
+        if (!l2) return l1;
+        ListNode* head = l1->val <= l2->val? l1 : l2;
+        head->next = l1->val <= l2->val ? merge2Lists(l1->next, l2) : merge2Lists(l1, l2->next);
+        return head;
+    }
+    
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if (lists.size() == 0) return NULL;
+        
+        ListNode* head = lists[0];
+        
+        for (int i = 1; i < lists.size(); i++)
+            head = merge2Lists(head, lists[i]);
+        
+        return head;
+    }
+};
+
+class Solution {
+    struct compare {
+        bool operator()(const ListNode* l, const ListNode* r) {
+            return l->val > r->val;
+        }
+    };
+    
+public:
+    ListNode *mergeKLists(vector<ListNode *> &lists) {
+        priority_queue<ListNode *, vector<ListNode *>, compare> q;
+        for (auto l : lists) {
+            if (l) {
+                q.push(l);
+            }
+        }
+        
+        if (q.empty()) {
+            return NULL;
+        }
+        
+        ListNode* result = q.top();
+        q.pop();
+        if (result->next) {
+            q.push(result->next);
+        }
+
+        ListNode* tail = result;            
+        while (!q.empty()) {
+            tail->next = q.top();
+            q.pop();
+            tail = tail->next;
+            if (tail->next) {
+                q.push(tail->next);
+            }
+        }
+        
+        return result;
+    }
+};
+
