94f49ad3b514b4397163bdc1593eba8cd8355d4c
HansP101 <106307051+HansP101@users.noreply.github.com>
2022-10-03 16:25:44 -0700

    Create 416. Partition Equal Subset Sum.c

---
+++ 416. Partition Equal Subset Sum.c
@@ -0,0 +1,32 @@
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
+class Solution {
+public:
+    bool canPartition(vector<int> &nums) {
+        int totalSum = 0;
+        // find sum of all array elements
+        for (int num : nums) {
+            totalSum += num;
+        }
+        // if totalSum is odd,it cannot be partitioned into equal sum subset
+        if (totalSum % 2 != 0) return false;
+        int subSetSum = totalSum / 2;
+        int n = nums.size();
+        return dfs(nums, n - 1, subSetSum);
+    }
+
+    bool dfs(vector<int> &nums, int n, int subSetSum) {
+        // Base Cases
+        if (subSetSum == 0)
+            return true;
+        if (n == 0 || subSetSum < 0)
+            return false;
+        bool result = dfs(nums, n - 1, subSetSum - nums[n - 1]) || dfs(nums, n - 1, subSetSum);
+        return result;
+    }
+};
