8a490788f3d01d6d703c49d4815cdc0537f6fa3c
HansP101 <106307051+HansP101@users.noreply.github.com>
2022-10-04 19:45:55 -0700

    Update 234. Palindrome Linked List.c

--- 234. Palindrome Linked List.c
+++ 234. Palindrome Linked List.c
@@ -21,3 +21,32 @@ public:
         return isPal;
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        stack<int> listStack;
+        ListNode* temp=head;
+        while(head){
+            listStack.push(head->val);
+            head=head->next;
+        }
+        while(temp){
+            if(temp->val!=listStack.top()) return false;
+            temp=temp->next;
+            listStack.pop();
+        }
+        if(listStack.empty()) return true;
+        else return false;
+    }
+};
