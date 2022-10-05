73ae56ceab00b24241bd31af26bd75de0572da6b
HansP101 <106307051+HansP101@users.noreply.github.com>
2022-10-05 14:00:40 -0700

    Update 1004-MaxConsecutiveOnesIII.c

--- 1004-MaxConsecutiveOnesIII.c
+++ 1004-MaxConsecutiveOnesIII.c
@@ -1,11 +1,12 @@
+//Max consecutive 1s when flip zero kth.
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
         int start=0, end=0; //sliding window
         int ans=0;
         for(end; end<nums.size(); end++){
-            if(nums[end]==0)k--;
-            while(k<0){
+            if(nums[end]==0)k--; //flipped
+            while(k<0){ //keep it up start to make # of flipped is limitted to k'th 
                 if(nums[start]==0)k++;
                 start++;}            
             int temp=end-start+1;
