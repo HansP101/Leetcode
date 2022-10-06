99c2449eb3cf7e084e008ce7b18f93a7344cbafa
HansP101 <106307051+HansP101@users.noreply.github.com>
2022-10-05 22:10:49 -0700

    Update 22. Generate Parentheses.c

--- 22. Generate Parentheses.c
+++ 22. Generate Parentheses.c
@@ -9,7 +9,7 @@ public:
     void DPbackTrackingHelper(int n, int l, int r, string current, vector<string>&ans){
         if(l==n and r==n) ans.push_back(current);
         else{
-            if(l<n) DPbackTrackingHelper(n, l+1, r, current+'(', ans);
+            if(l<n) DPbackTrackingHelper(n, l+1, r, current+'(', ans); //first let's increase l to all the way and backtracking will kick in
             if(r<l) DPbackTrackingHelper(n, l, r+1, current+')', ans);
         }
     }
