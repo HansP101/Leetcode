37459ee1933f1087d5f64a1c66487b6acbf9c3dc
HansP101 <106307051+HansP101@users.noreply.github.com>
2022-10-24 16:42:55 -0700

    Update 70. Climbing Stairs.c

--- 70. Climbing Stairs.c
+++ 70. Climbing Stairs.c
@@ -48,3 +48,18 @@ public:
 
     }
 };
+
+//10/24/2022
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n+2, 0);
+        dp[0]=1;
+        for(int i=0; i<n; i++){
+            dp[i+1]+=dp[i];
+            dp[i+2]+=dp[i];
+        }
+        return dp[n];
+    }
+};
