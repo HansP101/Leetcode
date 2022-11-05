ff992fad1e854f0a22d9f2b3b287fd1b7e2c182d
HansP101 <106307051+HansP101@users.noreply.github.com>
2022-11-05 15:29:35 -0700

    Update 371 Sum of Two Integers.c

--- 371 Sum of Two Integers.c
+++ 371 Sum of Two Integers.c
@@ -12,3 +12,21 @@ public:
         
     }
 };
+
+class Solution {
+public:
+    int getSum(int a, int b) {
+        int ans=0;
+        uint32_t carryOver=0;
+        int bitPos=0;
+        carryOver=a&b;
+        ans=a^b;
+        while(carryOver){
+            carryOver=carryOver<<1;
+            int temp=carryOver^ans;
+            carryOver=carryOver&ans;
+            ans=temp;
+        }
+        return ans;
+    }
+};
