f43a7c84fac18eb9d2d93ba02d235c41f14a7fb0
HansP101 <106307051+HansP101@users.noreply.github.com>
2022-11-05 15:51:13 -0700

    Update 190. Reverse Bits.c

--- 190. Reverse Bits.c
+++ 190. Reverse Bits.c
@@ -9,6 +9,20 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t ans=0;
+        int i=32;
+        while(i--){
+            ans<<=1;
+            ans|=n&0x1;
+            n>>=1;     
+        }
+        return ans;
+    }
+};
 /*
 class Solution {
 public:
