ef8db54df80bf1017deede898908d829b2d86210
HansP101 <106307051+HansP101@users.noreply.github.com>
2022-12-01 00:10:52 -0800

    Update 92. Reverse Linked List II.c

--- 92. Reverse Linked List II.c
+++ 92. Reverse Linked List II.c
@@ -9,6 +9,23 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+ 
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int m, int n) {
+    ListNode **pre = &head;
+    int steps = m;
+    while (--steps) { pre = &(*pre)->next;}
+    ListNode *cur = *pre;
+    for (int i = m; i < n; i++) {
+        ListNode *next_n = cur->next;
+        cur->next = next_n->next;
+        next_n->next = *pre;
+        *pre = next_n;
+    }
+    return head; 
+  }
+};
 class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
