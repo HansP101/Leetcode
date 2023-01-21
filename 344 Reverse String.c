636bd39cdbdc90bf3800e12a20c0f5d756f12b83
HansP101 <106307051+HansP101@users.noreply.github.com>
2023-01-20 19:40:04 -0800

    Update 344 Reverse String.c

--- 344 Reverse String.c
+++ 344 Reverse String.c
@@ -11,3 +11,23 @@ public:
 };
 
 // not to use reverse(s.begin(), s.end());
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void reverseString(string &inStr){
+  for(int i=0,j=inStr.size()-1;i<j; i++, j--){
+      swap(inStr[i], inStr[j]);
+  }
+}
+
+int main(){ 
+    string inStr="HELLO";
+    reverseString(inStr);
+    printf("%s",inStr.c_str());
+    return 0;
+}
